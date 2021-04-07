00d4f612658968f392fcf9e426dcddc395fc51bf
Nikolas Moatsos <nmoazzo@gmail.com>
2021-04-07 13:09:44 +0300

    Add Javadoc

--- ase2021_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ ase2021_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -10,43 +10,78 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/**
+ * <h1>Generate Histogram with Grades</h1>
+ * The HistogramGenerator takes as an input a file with Integer grades, from the
+ * command line and generates a histogram, using the JFreeChart charting library.
+ * @author Nikolas Moatsos
+ * @since April 2021
+ */
+
 public class HistogramGenerator {
 	
-	
+	/**
+	 * This method passes the values from the file to a static array of Integers.
+	 * @param input This is a file with Integer grades.
+	 * @return int[] This is an array with Integer grades.
+	 */
 	public int[] readGrades(Scanner input) {
 		
+		// Initialize the array.
 		int grades[] = new int[149];
 		
+		// Pass the grades from the file to the array.
 		for (int i = 0; i < 149; i++) {
 			grades[i] = input.nextInt();
 		}
 		
+		// Return the array.
 		return grades;
 	}
 	
-	
+	/**
+	 * This method visualizes an array with Integer grades with a histogram
+	 * from the JFreeChart charting library.
+	 * @param grades This is an array with Integer grades.
+	 */
 	public void generateHistogram(int[] grades) {
 		
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that
+		 * can be visualized in the same chart.
+		 */
 		XYSeriesCollection dataset = new XYSeriesCollection();
 		
+		// The XYSeries that are loaded in the dataset.
 		XYSeries data = new XYSeries("grades");
 		
+		/*
+		 * Populating the XYSeries data object from the input Integer array
+		 * values.
+		 */
 		for (int i = 0; i < grades.length; i++) {
 			data.add(i, grades[i]);
 		}
 		
+		// Add the series to the dataset.
 		dataset.addSeries(data);
 		
-		boolean legend = false;
-		boolean tooltips = false; 
-		boolean urls = false;
+		boolean legend = false; // do not visualize a legend
+		boolean tooltips = false; // do not visualize tooltips
+		boolean urls = false; // do not visualize urls
 		
+		// Declare and initialize a createXYLineChart JFreeChart.
 		JFreeChart chart = ChartFactory.createXYLineChart("Grades Histogram", "Number of Grade", "Grade", dataset,
 				PlotOrientation.VERTICAL, legend, tooltips, urls);
 		
+		/*
+		 * Initialize a frame for visualizing the chart and attach the
+		 * previously created chart.
+		 */
 		ChartFrame frame = new ChartFrame("First", chart);
 		frame.pack();
 		
+		// Make the previously created frame visible.
 		frame.setVisible(true);
 		
 	}
@@ -54,8 +89,11 @@ public class HistogramGenerator {
 	
 	public static void main (String args[]) {
 		
+		// Initialize the Scanner. 
 		Scanner input = null;
 		
+
+		// Read the input file from the command line.
 		try {
 			input = new Scanner(new File(args[0]));
 		} catch (FileNotFoundException e) {
@@ -63,6 +101,7 @@ public class HistogramGenerator {
 			return;
 		}
 		
+		// Generate the histogram.
 		HistogramGenerator gen = new HistogramGenerator();
 		int grades[] = gen.readGrades(input);
 		gen.generateHistogram(grades);
