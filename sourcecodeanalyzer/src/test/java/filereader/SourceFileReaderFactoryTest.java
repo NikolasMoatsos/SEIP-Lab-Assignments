3b2ea8f7ea00aaf96b2ad56e75eec8caacd5addd
nmoatsos <56679780+NikolasMoatsos@users.noreply.github.com>
2021-06-12 17:48:16 +0300

    Create test cases for the filereader package

---
+++ sourcecodeanalyzer/src/test/java/filereader/SourceFileReaderFactoryTest.java
@@ -0,0 +1,25 @@
+package filereader;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class SourceFileReaderFactoryTest {
+    
+    SourceFileReaderFactory sfrf = new SourceFileReaderFactory();
+
+    @Test
+    public void testLocalFileReader() {
+		assertThat(sfrf.createSourceFileReader("local"), instanceOf(LocalFile.class));
+	}
+
+    @Test
+    public void testWebFileReader() {
+		assertThat(sfrf.createSourceFileReader("web"), instanceOf(WebFile.class));
+	}
+
+    @Test
+    public void testWrongFileReader() {
+		assertThat(sfrf.createSourceFileReader("wrong"), instanceOf(NullFile.class));
+	} 
+}
